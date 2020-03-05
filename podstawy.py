28585c41bc8edd86aba79f14f9d05016e989a8d9
Arkadiusz Kopczyński <akopczynski@edu.cdv.pl>
2020-03-05 17:20:25 +0100

    Zmiany

--- podstawy.py
+++ podstawy.py
@@ -1,1 +1,37 @@
 print('test')
+
+#print('test')
+
+#print("Janusz")
+
+#test=input()
+
+
+'''
+print("test", end="")
+jan=input()
+
+
+age=20
+print(type(age))
+print("Twoj wiek", age, " lata")
+'''
+#####################
+
+surname="Kowalski"
+firstChar = surname[0]
+print(firstChar)
+
+print(surname[-1])
+
+
+#KONWERSJA
+
+x ="5"
+print(type(x))
+
+print(type(int(x)))
+
+x=x/2
+
+#print(type(x))
