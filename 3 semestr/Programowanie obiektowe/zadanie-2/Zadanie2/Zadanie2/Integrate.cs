b702d88667be79e09985253322cce517f3fe6d6e
Arek <itarekko@gmail.com>
2020-11-04 16:15:55 +0100

    Adding progess on ex.2

---
+++ 3 semestr/Programowanie obiektowe/zadanie-2/Zadanie2/Zadanie2/Integrate.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zadanie2
+{
+    class Integrate
+    {
+        //Properties gets from obejct initiation
+        private int _functionType = 0;
+        private int _intervalStart = 0;
+        private int _intervalStop = 0;
+        
+        //Working properties
+        private double _widthOfPart = 0;
+        public double _result = 0;
+
+        public Integrate(int functionType)
+        {
+            _functionType = functionType;
+            _intervalStart = establishIntervalStart();
+            _intervalStop = establishIntervalStop();
+        }
+
+        //The Core method
+        public bool calculate()
+        {
+
+            calculateWidthOfPart();
+
+            double result = 0;
+
+            switch(getFunctionType())
+            {
+                case 1:
+                    Console.WriteLine("Podaj wartość potęgi");
+                    int power = Program.getDigitFromUser();
+
+                    Console.WriteLine("Podaj wartość wyrazu wolnego b");
+                    int b = Program.getDigitFromUser();
+
+                    for (int i = 1; i <= 3; i++)
+                    {
+                        result += calculateUserFunctionType1(getIntervalStart() + i * getWidthOfPart(), power, b);
+
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            result = getWidthOfPart() * result; 
+
+            setResult(result); 
+
+            return true; 
+        }
+
+        private bool calculateWidthOfPart()
+        {
+
+            double calculatedWidthOfPart = (getIntervalStop() - getIntervalStart()) / 3;
+
+            setWidthOfPart(calculatedWidthOfPart);
+
+            return true; 
+
+        }
+
+        private double calculateUserFunctionType1(double x, int power, int b)
+        {
+            return Math.Pow(x, power) + b;
+        }
+
+        private int getFunctionType()
+        {
+            return _functionType;
+        }
+
+        private int getIntervalStart()
+        {
+            return _intervalStart;
+        }
+
+        private int getIntervalStop()
+        {
+            return _intervalStop;
+        }
+
+        public double getResult()
+        {
+            return _result;
+        }
+        private double getWidthOfPart()
+        {
+            return _widthOfPart;
+        }
+
+        private int establishIntervalStart()
+        {
+            Console.WriteLine("Podaj początek zakresu całkowania");
+            return Program.getDigitFromUser();
+        }
+
+        private int establishIntervalStop()
+        {
+            Console.WriteLine("Podaj koniec zakresu całkowania");
+            return Program.getDigitFromUser();
+        }
+
+        private void setWidthOfPart(double widthOfPart)
+        {
+            _widthOfPart = widthOfPart;
+        }
+
+        private void setResult(double result)
+        {
+            _result = result;
+        }
+    }
+}
---
+++ 3 semestr/Programowanie obiektowe/zadanie-2/Zadanie2/Zadanie2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Zadanie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice = 0;
+
+            do
+            {
+                Console.WriteLine("Wybierz typ funkcji\n0. Koniec programu\n1. f(x)=x^2+b\n2. f(x)=3x^2+b");
+
+                choice = getDigitFromUser();
+
+                switch (choice)
+                {
+                    case 1:
+                        Integrate integrate = new Integrate(choice);
+
+                        if (!integrate.calculate())
+                            Console.WriteLine("BLAD");
+
+                        Console.WriteLine("WYNIK: " + integrate.getResult());
+                        break;
+                    default:
+                        break;
+                }
+            } while (choice != 0);
+        }
+
+        public static int getDigitFromUser()
+        {
+            int digit = 0;
+
+            while (!int.TryParse(Console.ReadLine(), out digit))
+            {
+                Console.WriteLine("Niepoprawną wartość. Podaj liczbe");
+            }
+
+            return digit;
+        }
+    }
+}
